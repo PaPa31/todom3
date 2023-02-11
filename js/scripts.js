50fe466bf8d48006e2c80297aab161356569b395
papa <visidoor@yandex.ru>
2023-02-11 18:56:31 +0500

    integrating localStorage

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ const form = document.querySelector("form");
 const ul = document.querySelector("ul");
 const button = document.querySelector("button");
 const input = document.getElementById("item");
+const itemsArray = [];
+
+localStorage.setItem("items", JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem("items"));
 
 const liMaker = (text) => {
   const li = document.createElement("li");
@@ -12,6 +16,20 @@ const liMaker = (text) => {
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
+  itemsArray.push(input.value);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+
   liMaker(input.value);
   input.value = "";
+});
+
+data.forEach((item) => {
+  liMaker(item);
+});
+
+button.addEventListener("click", function () {
+  localStorage.clear();
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
 });
