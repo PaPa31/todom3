e165c1949258bf6a00f6f8260c9e61104f04ea78
papa <visidoor@yandex.ru>
2023-04-11 19:11:46 +0500

    chore(files): add handler if file:/// protocol

--- js/states.js
+++ js/states.js
@@ -1,6 +1,11 @@
 const firstHeaderButton = document.getElementById("first-header");
 const secondHeaderButton = document.getElementById("second-header");
 
+//const fileSelect = document.getElementById("fileSelect");
+const fileElem = document.getElementById("fileElem");
+//const fileList = document.getElementById("fileList");
+//fileSend = document.getElementById("sendFile");
+
 // starting in Item state & Unfolded view
 let isFileState = false;
 let isFoldedView = false;
@@ -104,9 +109,65 @@ const logFileText = async (file) => {
   ol.innerHTML = text;
 };
 
+function handleFiles() {
+  const [file] = document.querySelector("input[type=file]").files;
+
+  if (!this.files.length) {
+    ol.innerHTML = "<p>No files selected!</p>";
+  } else {
+    ol.innerHTML = "";
+    //const list = document.createElement("ul");
+    //ol.appendChild(list);
+    for (let i = 0; i < this.files.length; i++) {
+      const reader = new FileReader();
+
+      reader.addEventListener(
+        "load",
+        () => {
+          // this will then display a text file
+          //const li = document.createElement("li");
+          //li.innerText = reader.result;
+          //ol.appendChild(li);
+          liMaker(reader.result);
+        },
+        false
+      );
+
+      if (this.files[i]) {
+        reader.readAsText(this.files[i]);
+      }
+
+      //const img = document.createElement("img");
+      //img.src = URL.createObjectURL(this.files[i]);
+      //img.height = 60;
+      //img.onload = () => {
+      //  URL.revokeObjectURL(img.src);
+      //};
+      //li.appendChild(img);
+      //const info = document.createElement("span");
+      //info.innerHTML = `${this.files[i].name}: ${this.files[i].size} bytes`;
+      //li.appendChild(info);
+    }
+  }
+}
+
+fileElem.addEventListener("change", handleFiles, false);
+
 const initializeFileState = () => {
   ol.innerHTML = "";
-  logFileText(phrase);
+
+  console.log(window.location.protocol);
+
+  if (window.location.protocol === "file:") {
+    ol.innerHTML = ``;
+
+    if (fileElem) {
+      fileElem.click();
+    }
+    //e.preventDefault(); // prevent navigation to "#"
+  } else {
+    logFileText(phrase);
+  }
 };
 
 const fileState = () => {
