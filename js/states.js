e7037100b159e7e6530c580984ec471da2ff649e
papa <visidoor@yandex.ru>
2023-06-13 22:57:20 +0500

    fix: set the top dynamically to retain the scroll position

--- js/states.js
+++ js/states.js
@@ -184,9 +184,24 @@ const unfoldOneItem = (element) => {
     const itemIndexToFold = indexedItemsArray.indexOf(element.id) * 1;
     itemsArray[itemIndexToFold].fold = !itemsArray[itemIndexToFold].fold;
     localStorage.setItem("todomItemsArray", JSON.stringify(itemsArray));
+    // Facebook solution
+    // set the top dynamically to retain the scroll position
+    // https://stackoverflow.com/a/45230674
+    if (itemsArray[itemIndexToFold].fold) {
+      const scrollPosition = window.scrollY;
+      element.style.top = -scrollPosition + "px";
+    } else {
+      window.scrollTo(0, scrollPosition);
+    }
   } else {
     const fileIndexToFold = indexedFilesArray.indexOf(element.id) * 1;
     filesArray[fileIndexToFold].fold = !filesArray[fileIndexToFold].fold;
+    if (filesArray[fileIndexToFold].fold) {
+      const scrollPosition = window.scrollY;
+      element.style.top = -scrollPosition + "px";
+    } else {
+      window.scrollTo(0, scrollPosition);
+    }
   }
 };
 
