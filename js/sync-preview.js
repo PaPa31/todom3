aabb8d9f26a2e5aa1b52a007bdbd3525ed952cae
papa <visidoor@yandex.ru>
2023-06-14 11:47:01 +0500

    turn on loggs

--- js/sync-preview.js
+++ js/sync-preview.js
@@ -274,7 +274,7 @@ if (true) {
 
   // managing vars
   // change to show/hide output loggs
-  var showLogg = false; // logg - 'headAndTail'
+  var showLogg = true; // logg - 'headAndTail'
   var showLogg1 = false; // logg1 - 'whatClass'
   var showLogg2 = true; // logg2 -
   var showLogg3 = false; // logg3 -
