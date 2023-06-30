372cc47e9e3e0213cdefa2c22c34aee41553de63
papa <visidoor@yandex.ru>
2023-06-30 10:36:57 +0500

    fix: don't show x button when start

--- js/states.js
+++ js/states.js
@@ -72,11 +72,12 @@ const liMaker = (count) => {
   const li = document.createElement("li");
   const div = document.createElement("div");
   const textArr = itemsArray[count].text;
-  const len = textArr.length - 1;
+  const len = textArr.length;
+  const last = len - 1;
   const cur = itemsArray[count].cur;
-  const current = cur !== undefined ? cur : len;
+  const current = cur !== undefined ? cur : last;
   if (isItemState) {
-    const text = textArr[current] ? textArr[current] : textArr[len];
+    const text = textArr[current] ? textArr[current] : textArr[last];
 
     div.setAttribute("class", "md-item");
     if (itemsArray[count].fold) li.setAttribute("class", "unfolded");
@@ -116,12 +117,11 @@ const liMaker = (count) => {
 const saveHistoryDivMaker = (parentDiv, lengthSaveHistory, current) => {
   const divTag = document.createElement("div");
   divTag.setAttribute("id", "save-history");
-  if (lengthSaveHistory === 0) divTag.setAttribute("disable", true);
   parentDiv.appendChild(divTag);
 
   previousSaveButtonMaker(divTag, current);
   deleteCurrentSaveButtonMaker(divTag);
-  nextSaveButtonMaker(divTag, current === lengthSaveHistory);
+  nextSaveButtonMaker(divTag, current === lengthSaveHistory - 1);
 };
 
 const controlDivMaker = (parentDiv) => {
