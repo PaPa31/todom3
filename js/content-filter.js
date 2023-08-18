79a37c18a73820c10840a544351d12a677fcb2eb
papa <visidoor@yandex.ru>
2023-08-18 10:18:38 +0500

    fix: http doesn't work

--- js/content-filter.js
+++ js/content-filter.js
@@ -1,3 +1,22 @@
+const $ = (selector = "", scope = document) => {
+  const el =
+    scope && scope.querySelector && selector
+      ? scope.querySelector(selector)
+      : null;
+  if (el) {
+    return el;
+  } else {
+    throw new Error(`Cannot find element matching selector: ${selector}`);
+  }
+};
+
+const createEl = (tag, attr, pa) => {
+  const el = document.createElement(tag);
+  for (const key in attr) el.setAttribute(key, attr[key]);
+  if (pa) pa.appendChild(el);
+  return el;
+};
+
 const filterVideoIdFromUrl = (url) => {
   var video_id, result;
   if ((result = url.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/))) {
