6054b81e7646d06297f2b4ce0ad391a5abdc66cc
papa <visidoor@yandex.ru>
2023-08-23 13:25:00 +0500

    add height compensation on fold/unfold

--- js/states.js
+++ js/states.js
@@ -428,8 +428,18 @@ const foldGreen = (liDOM) => {
   }
 };
 
-const foldOneItem = (liDOM) => {
+const foldAndOffsetHeight = (liDOM) => {
+  const initialScrollTop = window.scrollY;
+  const initialScrollHeight = liDOM.scrollHeight;
   liDOM.classList.toggle("folded");
+  const newScrollHeight = liDOM.scrollHeight;
+  const layoutHeightIncrease = newScrollHeight - initialScrollHeight;
+  window.scrollTo(0, initialScrollTop);
+  console.log(`Layout height increased by ${layoutHeightIncrease}px`);
+};
+
+const foldOneItem = (liDOM) => {
+  foldAndOffsetHeight(liDOM);
   if (isItemState) {
     const itemIndexToFold = indexedItems.indexOf(liDOM.id) * 1;
     itemsSpecArray[itemIndexToFold].fold =
