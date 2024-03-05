a17d654c26bc44bb7cc772701dc2a1b442cabe8a
papa31 <visidoor@yandex.ru>
2024-03-05 19:16:23 +0500

    two button with input file API

--- server.js
+++ server.js
@@ -36,35 +36,65 @@ app.get("/", async (req, res) => {
 
 app.get("/open-directory", async (req, res) => {
   try {
-    const directoryPath = req.query.path;
+    const directoryPath = req.query.path || "";
     const fullPath = path.join(__dirname, directoryPath);
-    const files = await fs.readdir(fullPath);
+    const stats = await fs.stat(fullPath);
 
-    const htmlContent = `
-      <h1>${directoryPath}</h1>
-      <button onclick="window.history.back()">Go Back</button>
-      <ul>
-        ${files
-          .map(
-            (file) =>
-              `<li><a href="/open-directory?path=${encodeURIComponent(
-                path.join(directoryPath, file)
-              )}">${file}</a></li>`
-          )
-          .join("")}
-      </ul>
-    `;
+    if (stats.isDirectory()) {
+      const files = await fs.readdir(fullPath);
 
-    res.writeHead(200, {
-      "Content-Type": "text/html",
-    });
+      const htmlContent = `
+        <h1>${directoryPath}</h1>
+        <button onclick="window.history.back()">Go Back</button>
+        <ul>
+          ${files
+            .map(
+              (file) =>
+                `<li><a href="/open-directory?path=${path.join(
+                  directoryPath,
+                  file
+                )}">${file}</a></li>`
+            )
+            .join("")}
+        </ul>
+      `;
 
-    res.end(htmlContent);
+      res.send(htmlContent);
+    } else {
+      // If it's a file, directly open it
+      const fileContent = await fs.readFile(fullPath, "utf-8");
+
+      const ext = path.extname(fullPath).substring(1).toLowerCase();
+      const mimeType = getMimeType(ext);
+
+      res.writeHead(200, {
+        "Content-type": mimeType + "; charset=UTF-8",
+      });
+
+      res.end(fileContent);
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
   }
 });
+
+function getMimeType(ext) {
+  const MIME_TYPES = {
+    default: "application/octet-stream",
+    html: "text/html",
+    js: "application/javascript",
+    css: "text/css",
+    png: "image/png",
+    jpg: "image/jpg",
+    gif: "image/gif",
+    ico: "image/x-icon",
+    svg: "image/svg+xml",
+    json: "application/json",
+    md: "text/markdown",
+  };
+  return MIME_TYPES[ext] || MIME_TYPES.default;
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://127.0.0.1:${PORT}/`);
