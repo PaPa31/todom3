793e8e2b88e25314249df6cf329a67ce8b4ec5fd
papa31 <visidoor@yandex.ru>
2024-03-20 19:37:00 +0500

    server path to ".."

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "")));
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "..")));
 
 // Middleware function to log requested files
 app.use((req, res, next) => {
