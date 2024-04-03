8d191c81e4b642cb9da44ad84878f37f09a54ed8
papa31 <visidoor@yandex.ru>
2024-04-03 13:28:12 +0500

    replace word with svg

--- js/gallery.js
+++ js/gallery.js
@@ -13,7 +13,8 @@ function addButtonsAndWrapperToGalleries(liDOM) {
     // Create and append the Previous button
     const prevBtn = document.createElement("button");
     prevBtn.classList.add("prev-btn");
-    prevBtn.textContent = "Previous";
+    //prevBtn.textContent = "Previous";
+    prevBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/></svg>`;
     prevBtn.addEventListener("click", function () {
       galleryClone.scrollBy({
         left: -200, // Adjust scroll distance as needed
@@ -25,7 +26,8 @@ function addButtonsAndWrapperToGalleries(liDOM) {
     // Create and append the Next button
     const nextBtn = document.createElement("button");
     nextBtn.classList.add("next-btn");
-    nextBtn.textContent = "Next";
+    //nextBtn.textContent = "Next";
+    nextBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/></svg>`;
     nextBtn.addEventListener("click", function () {
       galleryClone.scrollBy({
         left: 200, // Adjust scroll distance as needed
