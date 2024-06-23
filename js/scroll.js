4d7bc8e14c250176d4a9e78d76ac246664e8d28a
papa31 <visidoor@yandex.ru>
2024-06-23 12:33:20 +0500

    preserve space

--- js/scroll.js
+++ js/scroll.js
@@ -6,9 +6,11 @@ function handleLiScroll(event) {
   const rect = li.getBoundingClientRect();
   const topVisible = rect.top >= 0 && rect.top <= window.innerHeight;
   const fullyVisible = rect.top >= 0 && rect.bottom <= window.innerHeight;
-  const bottomVisible = rect.bottom >= 0 && rect.bottom <= window.innerHeight;
 
-  // Add sticky class when the top of li element scrolls out of view upwards and bottom is not visible
+  // Calculate the height of the topDiv
+  const topInLiHeight = topInLi.getBoundingClientRect().height;
+
+  // Add sticky class and padding when the top of li element scrolls out of view upwards and bottom is not visible
   if (
     !topVisible &&
     !fullyVisible &&
@@ -17,21 +19,24 @@ function handleLiScroll(event) {
   ) {
     topInLi.classList.add("sticky");
     topInLi.style.width = `${li.clientWidth}px`; // Set the same width as li
+    li.style.paddingTop = `${topInLiHeight}px`; // Add padding to li
   }
 
-  // Remove sticky class when the bottom of li element scrolls out of view downwards
+  // Remove sticky class and padding when the bottom of li element scrolls out of view downwards
   if (rect.bottom <= 0) {
     topInLi.classList.remove("sticky");
     topInLi.style.width = ""; // Reset to default width
+    li.style.paddingTop = ""; // Remove padding
   }
 
-  // Remove sticky class when the top of li element becomes visible again
+  // Remove sticky class and padding when the top of li element becomes visible again
   if (topVisible || fullyVisible) {
     topInLi.classList.remove("sticky");
     topInLi.style.width = ""; // Reset to default width
+    li.style.paddingTop = ""; // Remove padding
   }
 
-  // Add sticky class to the next li when scrolling down and it appears at the top
+  // Add sticky class and padding to the next li when scrolling down and it appears at the top
   if (
     rect.bottom < window.innerHeight &&
     rect.bottom > 0 &&
@@ -40,6 +45,7 @@ function handleLiScroll(event) {
   ) {
     topInLi.classList.add("sticky");
     topInLi.style.width = `${li.clientWidth}px`; // Set the same width as li
+    li.style.paddingTop = `${topInLiHeight}px`; // Add padding to li
   }
 }
 
