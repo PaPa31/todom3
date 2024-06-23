82527573c3f1922a394f81c323d4a73781af61a4
papa31 <visidoor@yandex.ru>
2024-06-23 13:26:02 +0500

    fix: still sticky when scroll down through bottom edge

--- js/scroll.js
+++ js/scroll.js
@@ -32,7 +32,12 @@ function handleLiScroll(event) {
   }
 
   // Remove sticky class and padding when conditions are met
-  if (rect.top >= 0 || fullyVisible || belowHeightLimit) {
+  if (
+    rect.top >= 0 ||
+    rect.bottom <= window.innerHeight ||
+    fullyVisible ||
+    belowHeightLimit
+  ) {
     topInLi.classList.remove("sticky");
     topInLi.style.width = ""; // Reset to default width
     li.style.paddingTop = ""; // Remove padding
