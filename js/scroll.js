ccfb8fd025dbb74319493447bc1cdb15f626c79a
papa31 <visidoor@yandex.ru>
2024-06-24 14:34:08 +0500

    Updated Scroll Handling Logic

--- js/scroll.js
+++ js/scroll.js
@@ -2,28 +2,24 @@ const liHeightLimit = 300;
 let predictBottom = 100;
 let suspendTop = -200;
 
-let scrollLock = false;
-
-function handleLiScroll(event, callback) {
-  if (scrollLock) return;
-  scrollLock = true;
-
+// Function to handle scroll events on a specific li element
+function handleLiScroll(event) {
   const li = event.target;
   const topInLi = li.querySelector(".top-in-li");
-  if (!topInLi) {
-    scrollLock = false;
-    if (callback) callback();
-    return;
-  }
+  if (!topInLi) return; // Skip if there's no .top-in-li div
 
   const rect = li.getBoundingClientRect();
   const liHeight = li.clientHeight;
-  const topInLiHeight = topInLi.getBoundingClientRect().height;
+
+  // Calculate the height of the topDiv
+  // Manually set the required height to avoid jerking
+  const topInLiHeight = 39;
 
   const topVisible = rect.top >= 0 && rect.top <= window.innerHeight;
   const fullyVisible = rect.top >= 0 && rect.bottom <= window.innerHeight;
   const belowHeightLimit = liHeight < liHeightLimit;
 
+  // Logging variables for debugging
   console.log(`\nli \#${li.id}: --- Debug Info ---`);
   console.log(`liHeight: ${liHeight}`);
   console.log(`topInLiHeight: ${topInLiHeight}`);
@@ -31,6 +27,7 @@ function handleLiScroll(event, callback) {
   console.log(`rect.bottom: ${rect.bottom}`);
   console.log(`window.innerHeight: ${window.innerHeight}`);
 
+  // Apply sticky class and padding
   if (
     !belowHeightLimit &&
     rect.top < suspendTop &&
@@ -38,7 +35,7 @@ function handleLiScroll(event, callback) {
   ) {
     if (!topInLi.classList.contains("sticky")) {
       console.log("Adding sticky class");
-      li.style.paddingTop = `39px`; // fix mismatch jerking
+      li.style.paddingTop = `${topInLiHeight}px`; // Set the paddingTop first to avoid jerking
       topInLi.classList.add("sticky");
       topInLi.style.width = `${li.clientWidth}px`;
     }
@@ -51,6 +48,7 @@ function handleLiScroll(event, callback) {
     }
   }
 
+  // Remove sticky class and padding when the li is no longer in the viewport
   if (
     rect.bottom <= predictBottom &&
     !topVisible &&
@@ -64,17 +62,12 @@ function handleLiScroll(event, callback) {
       topInLi.style.width = "";
     }
   }
-
-  scrollLock = false;
-  if (callback) callback();
 }
 
 function addScrollListener(li) {
   const debouncedScrollHandler = debounce(
     () => {
-      handleLiScroll({ target: li }, () => {
-        // Optional callback after handling scroll
-      });
+      handleLiScroll({ target: li });
     },
     100,
     false
